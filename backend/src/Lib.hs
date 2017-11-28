84be9493bffeaf6b952536d51ec9d6d91b0e4315
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2017-11-28 14:35:35 +0100

    [Backend] AppendOnly file editing
    This way, errors can be handled gracefully

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Lib
     ( Lib.run
@@ -15,6 +17,7 @@ import           Servant
 import           System.IO
 import qualified Data.Map as Map
 import           Data.Time.Calendar
+import           Data.Time
 import           Control.Monad.IO.Class
 
 import           Control.Exception (IOException, try)
@@ -23,9 +26,12 @@ import qualified Data.ByteString.Lazy as BS
 import           Control.Monad (forever)
 import           Control.Concurrent (forkIO)
 import           Control.Concurrent.MVar
-import           System.IO.SafeWrite
 import           Network.Wai.Middleware.Cors
 import           Network.Wai.Middleware.Gzip
+import           Network.Socket (SockAddr)
+import Data.Monoid ((<>))
+import Data.Maybe (mapMaybe)
+import Data.Char (ord, chr)
 
 -- * app
 logger req s mi = do
@@ -42,12 +48,19 @@ run filename port = do
         defaultSettings
   runSettings settings =<< mkApp filename
 
+readDatabase :: BS.ByteString -> Calendar
+readDatabase s = toCal (mapMaybe decode' (BS.split (fromIntegral (ord '\n')) s))
+  where
+    toCal :: [LogLine] -> Calendar
+    -- We use the right biased behavior of Map.fromList to keep only the last value
+    toCal l = Calendar $ Map.fromList (map (\(LogLine day n _) -> (day, n)) l)
+
 mkApp :: String -> IO Application
 mkApp filename = do
   calendarS <- try @IOException $ BS.readFile filename
-  let cal = case decode' =<< hush calendarS of
-        Nothing -> Calendar (Map.empty)
-        Just c -> c
+  let cal = case calendarS of
+        Left _ -> Calendar (Map.empty)
+        Right s -> readDatabase s
 
   calVar <- newMVar cal
 
@@ -60,7 +73,7 @@ mkApp filename = do
 type CalendarApi =
   "calendar" :> Get '[JSON] [(Day, Int)] :<|>
   "month" :> Capture "year" Integer :> Capture "month" Int :> Get '[JSON] [(Day, Int)] :<|>
-  "update" :> Capture "day" Day :> Capture "halfdays" Int :> Get '[JSON] () :<|>
+  "update" :> Capture "day" Day :> Capture "halfdays" Int :> RemoteHost :> Get '[JSON] () :<|>
   Raw
 
 
@@ -81,14 +94,15 @@ getMonth c year month = do
       ret = filter (\(d, _) -> let (y, m, _) = toGregorian d in y == year && m == month) list
   pure ret
 
-updateCalendar :: String -> MVar Calendar -> Day -> Int -> Handler ()
-updateCalendar filePath c day n = do
+updateCalendar :: String -> MVar Calendar -> Day -> Int -> SockAddr -> Handler ()
+updateCalendar filePath c day n ip = do
+  t <- liftIO $ getCurrentTime
   liftIO $ modifyMVar_ c $ \cal -> do
     let newCal = updateDay day n cal
 
     -- save the db atomically
-    withOutputFile filePath $ \fp -> do
-      BS.hPutStr fp (encode newCal)
+    withFile filePath AppendMode $ \fp -> do
+      BS.hPutStr fp ((encode (LogLine day n (show ip <> " " <> show t))) <> "\n")
 
     pure newCal
 
@@ -96,3 +110,10 @@ updateCalendar filePath c day n = do
 
 updateDay day 0 (Calendar cal) = Calendar $ Map.delete day cal
 updateDay day n (Calendar cal) = Calendar $ Map.insert day n cal
+
+-- Do not change this API, the json is dumped to the file
+data LogLine = LogLine
+  Day -- ^ The updated day
+  Int -- ^ The worked number of half days [0-4]
+  String -- ^ A log line in any stupid format
+  deriving (Show, Generic, ToJSON, FromJSON)
